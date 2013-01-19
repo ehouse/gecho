6a7fbc9ec98d54d812a8b033272ccf8a14e6ad81
Ethan House <ewh2048@rit.edu>
2013-01-18 22:25:34 -0500

    Added semi functioning web

--- web/web/views.py
+++ web/web/views.py
@@ -1,6 +1,6 @@
 from pyramid.view import view_config
+from pyramid.response import Response
 
-
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'web'}
+@view_config(route_name='home', renderer='machineList.mak')
+def homeView(request):
+	return {'project': 'web','title':'gecho'}
