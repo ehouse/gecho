6690128ab8fc1c815e86286dfa5bf76c4a7d01f9
Will Dignazio <slackwill@csh.rit.edu>
2013-01-18 19:08:33 -0500

    spaces -> tabs

---
+++ gecho/proc.py
@@ -0,0 +1,9 @@
+"""
+Will Dignazio
+gecho 2012
+"""
+
+class ProcCPUEntry(object):
+	""" /proc/stat CPU Entry """
+	def __init__():
+		pass
--- setup.py
+++ setup.py
@@ -11,17 +11,16 @@ readme_fp.close()
 install_requires = [ ]
 
 setup(name="gecho",
-        version=version,
-        classifiers=[
-            "Programming Language :: Python :: 2.7",
-            "License :: OSI Approved :: GNU General Public License (GPL)",
-            "Intended Audience :: Developers"
-            ],
-        keywords="gecho monitor distributed",
-        author="Ethan House, Will Dignazio",
-        licence="GPLv2",
-        package=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-        author_email="slackwill@csh.rit.edu",
-        include_package_data=True,
-        zip_safe=False,
-        install_requires=install_requires)
+		version=version,
+		classifiers=[
+			"Programming Language :: Python :: 2.7",
+			"License :: OSI Approved :: GNU General Public License (GPL)",
+			"Intended Audience :: Developers"
+			],
+		keywords="gecho monitor distributed",
+		author="Ethan House, Will Dignazio",
+		license="GPLv2",
+		author_email="slackwill@csh.rit.edu",
+		include_package_data=True,
+		zip_safe=False,
+		install_requires=install_requires)
